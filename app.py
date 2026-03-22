288a78fc2a5ba58eb3e3deab88dc67ec41bac224
Anilkumar20102005 <anil20102005@gmail.com>
2026-03-22 20:06:02 +0530

    Fix render deployment

--- app.py
+++ app.py
@@ -528,5 +528,8 @@ with app.app_context():
     db.create_all()
     seed_database()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
